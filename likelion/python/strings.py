ae15646cc3645e979bb45a6cd23159d30304fa82
jaemoon <woans5970@naver.com>
2023-03-25 00:45:48 +0900

    study 2023-03-25

---
+++ likelion/python/strings.py
@@ -0,0 +1,29 @@
+# # 문자열(Strings) = 문자열 나열
+
+# city = "seoul" # Seoul, SEOUL, SEOul
+# print(city)
+
+# city.upper() # city에 함수가 적용된 것이 아니기 때문에
+# print(city)
+# print(city.upper()) # 전체를 다 적어줘야 한다
+
+# city = city.upper() # 재할당을 해준 경우는
+# print(city) # 변수 이름만 적어줘도 된다
+
+# city.lower()
+# print(city)
+# print(city.lower)
+
+# city = city.lower()
+# print(city)
+
+# occupation = "   developer   "
+# print(occupation)
+
+# occupation.rstrip()
+# print(occupation.rstrip()) # 오른쪽 공백 제거
+# print(occupation.lstrip()) # 왼쪽 공백 제거
+# print(occupation.strip()) # 양쪽 공백 제거
+
+print("INFP\nENFP\nISTJ\nESTJ") # \n = enter
+print("INFP\tENFP\tISTJ\tESTJ") # \t = tab
