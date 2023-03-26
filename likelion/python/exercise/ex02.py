cdc8b48130944ce73c497b59b878b7a6c373bdcb
jaemoon <woans5970@naver.com>
2023-03-26 16:25:53 +0900

    클래스 활용 예제

---
+++ likelion/python/exercise/ex02.py
@@ -0,0 +1,9 @@
+# ---------- 클래스 - 클래스 활용 ----------
+# from classes import Student, ForeignStudent
+from classes import *
+
+stud_1 = Student('김멋사', '컴퓨터공학과')
+foregin_stud_1 = ForeignStudent('이테킷', '국어국문학과', '미국')
+
+stud_1.study()
+foregin_stud_1.study()
