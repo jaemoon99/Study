41fa0749f69147a214dd789f3faa692d829073c4
jaemoon <woans5970@naver.com>
2023-04-28 15:36:53 +0900

    추억 점수

--- algorithm_study/memoryscore.py
+++ algorithm_study/memoryscore.py
@@ -1,14 +1,13 @@
 def solution(name, yearning, photo):
-    answer = []
-    total_score = 0
-    score_dic = dict(zip(name, yearning))
-    for x in range(len(photo)):
-        for y in photo[x]:
-            if y in name:
-                total_score += score_dic[y]
-        answer.append(total_score)
-        total_score = 0
-    return answer
+    answer = [] # 답안 배열
+    total_score = 0 # 사진 당 총 점수
+    score_dic = dict(zip(name, yearning)) # 이름과 그리움 점수 배열을 딕셔너리로 병합
+    for x in range(len(photo)): # 사진 루프
+        for y in photo[x]: # 사진 속 사람 루프
+            if y in name: # 사진 속 사람이 이름 배열에 있는지 확인
+                total_score += score_dic[y] # 있으면 총 점수에 덧셈
+        answer.append(total_score) # 이름 배열에 있는 사람들의 총 그리움 점수를 답안 배열에 저장
+        total_score = 0 # 다음 사진을 위해 총 점수 초기화
 
 name = ["may", "kein", "kain", "radi"]
 yearning = [5, 10, 1, 3]
