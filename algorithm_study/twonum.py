85d3936231b73bd2127c961aa51ef469704540d6
jaemoon <woans5970@naver.com>
2023-05-08 08:53:08 +0900

    두 개 뽑아서 더하기

--- algorithm_study/twonum.py
+++ algorithm_study/twonum.py
@@ -1,12 +1,7 @@
 def solution(numbers):
     answer = []
-    for x in range(0, len(numbers)):
+    for x in range(0, len(numbers)): # 각각 인덱스끼리 만날 수 있게 중첩 for문 사용
         for y in range(0, len(numbers)):
-            if x != y:
-                answer.append(numbers[x] + numbers[y])
-    return  sorted(list(set(answer)))
-
-
-numbers = [2,1,3,4,1]
-
-solution(numbers)
+            if x != y: # 다른 인덱스일 경우
+                answer.append(numbers[x] + numbers[y]) # 추가
+    return  sorted(list(set(answer))) # 같은 숫자가 있을 수 있기 때문에 중복제거 후 정렬
