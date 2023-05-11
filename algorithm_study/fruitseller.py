969351bb270b0a602e6684e246305ba384471885
jaemoon <woans5970@naver.com>
2023-05-12 01:02:06 +0900

    과일 장수

--- algorithm_study/fruitseller.py
+++ algorithm_study/fruitseller.py
@@ -1,3 +1,4 @@
+# 리스트 균등분할 함수
 def listsplit(score, n):
     list = []
     for x in range(0, len(score), n):
@@ -6,8 +7,11 @@ def listsplit(score, n):
 
 def solution(k, m, score):
     answer = 0
+    # 리스트 균등 분할
     chunk = listsplit(sorted(score, reverse=True), m)
+    # 분할한 각각의 부분 리스트 순회
     for x in chunk:
+        # 각 부분리스트의 원소의 개수가 m개인지 판단
         if len(x) == m:
             answer += x[-1] * m
     return answer
