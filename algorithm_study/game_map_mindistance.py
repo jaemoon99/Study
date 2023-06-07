7a91ee2c74e44075597c435a924f67085698759d
jaemoon <woans5970@naver.com>
2023-06-07 11:44:14 +0900

    비밀지도

--- algorithm_study/game_map_mindistance.py
+++ algorithm_study/game_map_mindistance.py
@@ -6,14 +6,15 @@ def solution(maps):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    queue = deque()
+    queue = []
     queue.append((0, 0))
 
     # queue가 빌 때까지 반복
     while queue:
         print(queue)
-        x, y = queue.popleft()
-
+        x = queue[0][0]
+        y = queue[0][1]
+        queue.remove(queue[0])
         # 상하좌우 칸 확인하기
         for i in range(4):
             nx = x + dx[i]
@@ -31,4 +32,38 @@ def solution(maps):
     answer = maps[len(maps)-1][len(maps[0])-1]
     return -1 if answer == 1 else answer    # 상대 팀 진영에 도착할 수 없을 때 -1
 
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+
+
+def solution2(maps):
+    answer = 0
+
+    # 상하좌우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    queue = deque()
+    queue.append((0, 0))
+
+    # queue가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        print(maps)
+        # 상하좌우 칸 확인하기
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 맵을 벗어나거나 벽이면 무시하기
+            if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]) or maps[nx][ny] == 0: continue
+
+            # 처음 지나가는 길이면 거리계산하고 다시 상하좌우 확인하기
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))    # 재귀
+
+    # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
+    answer = maps[len(maps)-1][len(maps[0])-1]
+    print(answer)
+    return -1 if answer == 1 else answer    # 상대 팀 진영에 도착할 수 없을 때 -1
+
+print(solution2([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
